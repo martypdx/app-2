f5ae71e1ae069efe90dee2641e8040318ad1f878
Ivy Patton <lilaivy@gmail.com>
2017-06-14 11:54:46 -0700

    changed landing page to dumb function and working on nav bar

--- src/components/Main/LandingPage.js
+++ src/components/Main/LandingPage.js
@@ -1,30 +1,30 @@
-import React, { Component } from 'react';
+import React from 'react';
 import { Switch, Route, Link, withRouter, Redirect } from 'react-router-dom';
 import PlaidAccountLink from '../Plaid/PlaidAccountLink';
 import saveYourWay from '../../photos/saveYourWay.jpg';
+import LandingPage from '../css/LandingPage.css';
 
-class LandingPage extends Component {
-  render() {
-    return (
-      <div className="Home">
-        <img src={saveYourWay} alt="backpack" />
-        <div className="welcomeHome">
-          <h2 className="dash-trailing">This Is Good Cents.</h2>
-          <p>One taste silent disco beltane, patchouli retreat. Tahini Hafiz tofu, ancient toxins rites of passage surrender chia seeds natural homebirth om Dr. Bronner's. Doula ecstatic dance transformative solstice third eye midwifery higher cosmic force medical marijuana papyrus font, organic raw cacao. Namaste. A Simple Shared account helps you and your partner easily budget, save, and talk about money. In good financial partnership, one plus one can equal anything.</p>
-        </div>
-        <p>
-          <Link className="navButton" to="auth/signin">
-            Sign In
-        </Link>
-        </p>
-        <p>
-          <Link className="navButton" to="auth/signup">
-            Create An Account
-        </Link>
-        </p>
+function LandingPage() {
+
+  return (
+    <div className="Home">
+      <img src={saveYourWay} alt="backpack" />
+      <div className="welcomeHome">
+        <h2 className="dash-trailing">This Is Good Cents.</h2>
+        <p>One taste silent disco beltane, patchouli retreat. Tahini Hafiz tofu, ancient toxins rites of passage surrender chia seeds natural homebirth om Dr. Bronner's. Doula ecstatic dance transformative solstice third eye midwifery higher cosmic force medical marijuana papyrus font, organic raw cacao. Namaste. A Simple Shared account helps you and your partner easily budget, save, and talk about money. In good financial partnership, one plus one can equal anything.</p>
       </div>
-    );
-  }
+      <p>
+        <Link className="navButton" to="auth/signin">
+          Sign In
+        </Link>
+      </p>
+      <p>
+        <Link className="navButton" to="auth/signup">
+          Create An Account
+        </Link>
+      </p>
+    </div>
+  );
 }
 
 export default LandingPage;
