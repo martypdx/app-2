37830e454b228f5385ca314b3cf0902c13eb3e1a
Ivy Patton <lilaivy@gmail.com>
2017-06-14 14:49:22 -0700

    render nav component based on size

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,68 +1,25 @@
 import React from 'react';
-import { Collapse, Navbar, NavbarToggler, NavbarBrand, Nav, NavItem, NavLink } from 'reactstrap';
-import 'bootstrap/dist/css/bootstrap.css';
-
-export default class Example extends React.Component {
-  constructor(props) {
-    super(props);
-
-    this.toggleNavbar = this.toggleNavbar.bind(this);
-    this.state = {
-      collapsed: true
-    };
-  }
-
-  toggleNavbar() {
-    this.setState({
-      collapsed: !this.state.collapsed
-    });
-  }
-  render() {
-    return (
-      <div>
-        <Navbar color="faded" light>
-          <NavbarToggler onClick={this.toggleNavbar} />
-          <Collapse className="navbar-toggleable-md" isOpen={!this.state.collapsed}>
-            <Nav navbar>
-              <NavItem>
-                <NavLink href="/">About</NavLink>
-              </NavItem>
-              <NavItem>
-                <NavLink href="/auth/signin">Log In</NavLink>
-              </NavItem>
-              <NavItem>
-                <NavLink href="/auth/signup">Create Account</NavLink>
-              </NavItem>
-            </Nav>
-          </Collapse>
-        </Navbar>
-      </div>
-    );
-  }
-}
-
-
-
-/*import React from 'react';
 import logoWhite from '../photos/logoWhite.png';
 import pigLogo from '../photos/pigLogo.png';
 import '../css/NavBar.css';
 import styled from 'styled-components';
+import MobileNav from './MobileNav';
 import { nameOfAnimation as Menu } from 'react-burger-menu';
 
-function NavBar() {
+export default function NavBar() {
+  if(window.innerWidth < 500) return <MobileNav/>;
+  
   return (
     <div className="NavBar">
-      <img src={pigLogo} className="navLogo" alt="logo" />
+      {/*<img src={pigLogo} className="navLogo" alt="logo" />*/}
       <ul className="ListContainer">
-        <li>Features</li>
-        <li>Blog</li>
+        <li>About</li>
         <li>Log In</li>
-        <li><button className="navButton">Apply Now</button></li>
+        <li>Learn More</li>
+        <li><button className="navButton">Create Account</button></li>
       </ul>
     </div>
   );
 }
 
 
-export default NavBar;*/
