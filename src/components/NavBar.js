9a90c605661b92b23506cd48439edea35b805ae5
Ivy Patton <lilaivy@gmail.com>
2017-06-14 15:18:04 -0700

    added opacity to nav bar for seemless scroll

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,23 +1,24 @@
 import React from 'react';
-import logoWhite from '../photos/logoWhite.png';
 import pigLogo from '../photos/pigLogo.png';
 import '../css/NavBar.css';
-import styled from 'styled-components';
 import MobileNav from './MobileNav';
+import { Link } from 'react-router-dom';
 import { nameOfAnimation as Menu } from 'react-burger-menu';
 
 export default function NavBar() {
-  if(window.innerWidth < 500) return <MobileNav/>;
-  
+  if (window.innerWidth < 500) return <MobileNav />;
+
   return (
-    <div className="NavBar">
-      {/*<img src={pigLogo} className="navLogo" alt="logo" />*/}
-      <ul className="ListContainer">
-        <li>About</li>
-        <li>Log In</li>
-        <li>Learn More</li>
-        <li><button className="navButton">Create Account</button></li>
-      </ul>
+    <div className="NavBar-Wrapper">
+      <div className="NavBar">
+        {/*<img src={pigLogo} className="navLogo" alt="logo" />*/}
+        <ul className="ListContainer">
+          <Link to="/"><li>About</li></Link>
+          <Link to="/auth/signin"><li>Log In</li></Link>
+          <Link to="/home"><li>Learn More</li></Link>
+          <Link to="/auth/signup"><li className="navButton">Create Account</li></Link>
+        </ul>
+      </div>
     </div>
   );
 }
