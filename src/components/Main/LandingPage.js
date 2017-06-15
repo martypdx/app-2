9adef9e0ef2b595294f39c013bb4c96132967f4c
Ivy Patton <lilaivy@gmail.com>
2017-06-15 11:33:08 -0700

    updated savings icons adjusted savings

--- src/components/Main/LandingPage.js
+++ src/components/Main/LandingPage.js
@@ -2,8 +2,8 @@ import React from 'react';
 import { Link } from 'react-router-dom';
 import saveYourWay from '../../photos/saveYourWay.jpg';
 import coffee from '../../photos/coffee.svg';
-import shoe from '../../photos/shoe.svg';
-import grocery from '../../photos/grocery.svg';
+import transport from '../../photos/transport.svg';
+import burger from '../../photos/burger.svg';
 import arrow from '../../photos/arrow.svg';
 import '../../css/LandingPage.css';
 
@@ -17,16 +17,17 @@ function LandingPage() {
         <h1 className="dash-trailing">This is Good Cents.</h1>
         <p className="landing-text">Let us turn your spare change into big savings....automatically!</p>
 
+        <span className="savings-wrapper">
+          <span ><img className="item-icons" src={coffee} /></span>
+          <span className="savings-text" >Daily Coffee:$3.25<img className="item-icons" src={arrow} />save .75 <img className="item-icons arrow" src={arrow} />$273 yearly savings</span><br></br>
 
-        <span ><img className="item-icons" src={coffee} /></span>
-        <span className="savings-text" >Daily Coffee:$3.25<img className="item-icons" src={arrow} />save .75 <img className="item-icons arrow" src={arrow} />$273 yearly savings</span><br></br>
-
-        <span ><img className="item-icons" src={shoe} /></span>
-        <span className="savings-text">Daily Coffee:$3.25<img className="item-icons arrow" src={arrow} />save .75 <img className="item-icons" src={arrow} />$273 yearly savings</span><br></br>
+          <span ><img className="item-icons" src={transport} /></span>
+          <span className="savings-text">Daily Commute:$2.75<img className="item-icons arrow" src={arrow} />save .25 <img className="item-icons" src={arrow} />$91 yearly savings</span><br></br>
 
 
-        <span ><img className="item-icons" src={grocery} /></span>
-        <span className="savings-text">Daily Coffee:$3.25<img className="item-icons arrow" src={arrow} />save .75 <img className="item-icons" src={arrow} />$273 yearly savings</span>
+          <span ><img className="item-icons burger" src={burger} /></span>
+          <span className="savings-text">Lunch:$7.50<img className="item-icons arrow" src={arrow} />save .50 <img className="item-icons" src={arrow} />$182 yearly savings</span>
+        </span>
 
         <div className="welcomeHome welcomeHome-alt">
           <h1 className="dash-trailing">Treat Yourself.</h1>
