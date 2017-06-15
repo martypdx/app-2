61f86f55885b22c6470d0aebddf563b086c1e881
Keeley Hammond <keeleymhammond@gmail.com>
2017-06-15 13:42:59 -0700

    Added transactions

--- src/components/HomeViews/PiggyBank.js
+++ src/components/HomeViews/PiggyBank.js
@@ -1,14 +1,18 @@
 import React from 'react';
+import piglogo from '../../photos/pigLogo.png'
 import { connect } from 'react-redux';
 
 function PiggyBank({ piggybank }) {
   return (
-    <div>${ piggybank.toFixed(2) }</div>
+    <div>
+      {/*<img src={piglogo} className="backpack" alt="backpack" />*/}
+      <div>${piggybank.toFixed(2)}</div>
+    </div>
   );
 }
 
 export default connect(
-  state => ({ 
+  state => ({
     user: state.user,
     transactions: state.transactions,
     piggybank: state.piggybank
