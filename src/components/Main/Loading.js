00bee4be7194df01c9f0ecfe78b4f16be44a2d23
Keeley Hammond <keeleymhammond@gmail.com>
2017-06-14 18:18:23 -0700

    Added loading animation to Loading component

--- src/components/Main/Loading.js
+++ src/components/Main/Loading.js
@@ -1,10 +1,12 @@
 import React from 'react';
+import Loader from 'halogen/PulseLoader';
 
 export default function Loading({ user, accounts }) {
   return (
     <div>
       <h2 style={{ color: 'red' }}>LOADING...</h2>
       <p style={{ color: 'red' }}>Average Load Time: 10 seconds</p>
+      <Loader color="#26A65B" size="16px" margin="4px"/>
     </div>
   );
 }
