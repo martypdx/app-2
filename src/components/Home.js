ce3b04e88bbf63514cae75d468b0383ebb0daaf3
Chris_Wallace <kawallis@icloud.com>
2017-06-15 14:58:56 -0700

    added trans and circle chart

--- src/components/Home.js
+++ src/components/Home.js
@@ -7,32 +7,24 @@ import PiggyBank from './HomeViews/PiggyBank';
 import { Redirect, withRouter } from 'react-router';
 import { connect } from 'react-redux';
 import { checkForToken } from './Main/actions';
-var ProgressBar = require('progressbar.js');
+
 
 class Home extends Component {
   constructor(props) {
     super(props);
 
-    this.barHandler = this.barHandler.bind(this);
-  }
-
-  barHandler() {
-    var circle = new ProgressBar.Line('.progress', {
-      color: '#FCB03C',
-      duration: 3000,
-      easing: 'easeOut'
-    });
-    circle.animate(1);
   }
 
   render() {
     const { user } = this.props;
+
     if (!user) return <Redirect to="/" />;
     return (
       <section>
         <div className="Home">
           <div className="PiggyContainer">
             <PiggyBank />
+            
           </div>
           <div className="UserContainer">
             <div className="UserBox"></div>
@@ -42,12 +34,11 @@ class Home extends Component {
             </div>
           </div>
           <div className="LineGraphContainer">
+            
           </div>
           <div className="TransactionsContainer">
+            <Transactions />
           </div>
-        </div>
-        <div>
-          <Transactions />
         </div>
       </section>
     );
