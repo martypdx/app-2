4f7a0ddad43ea1b88c4401dc3c500571f16ee45f
Ivy Patton <lilaivy@gmail.com>
2017-06-15 11:58:20 -0700

    updated fonts and language on onboarding carousel

--- src/components/Onboard.js
+++ src/components/Onboard.js
@@ -3,6 +3,7 @@ import Carousel from 'nuka-carousel';
 import Goals from '../photos/illustration-home-goal.png';
 import ATM from '../photos/illustrationATM.png';
 import WithYou from '../photos/illustrationWithYou.png';
+import piggyBank from '../photos/piggyBank.jpg';
 import PlaidAccountLink from './Plaid/PlaidAccountLink';
 import Loading from './Main/Loading';
 import { withRouter, Redirect } from 'react-router-dom';
@@ -19,13 +20,19 @@ function Onboard({ user, accounts, linking, location }) {
         <div>
           <img src={Goals} alt="Goals" />
           <h2>Welcome to Good Cents!</h2>
-          <p>Start using your digital "spare change" to save toward a goal, or contribute to something good.</p>
+          <p className="onboard-text">Start using your digital "spare change" to save toward a goal, or contribute to something good.</p>
         </div>
         <div>
           <img src={ATM} alt="ATM" />
           <h2>How it works:</h2>
-          <p>We'll start by linking one of your bank accounts or credit cards to Good Cents. As you make purchases throughout the week, we'll track your transactions, tally your change to the nearest dollar, and deposit it into our digital piggy bank.</p>
-          <p>When you hit the minimum threshold, cash out!</p>
+          <p className="onboard-text">Just link one of your bank accounts or credit cards to Good Cents....and let the savings being! </p>
+
+        </div>
+        <div>
+          <img src={piggyBank} alt="ATM" />
+          <h2>How it works:</h2>
+          <p className="onboard-text">As you make purchases throughout the week, we'll track your transactions, tally your change to the nearest dollar, and deposit the difference into our digital piggy bank.</p>
+          <p className="onboard-text">When you hit the $10 minimum savings threshold, cash out or save more!</p>
         </div>
         <div>
           <img src={WithYou} alt="WithYou" />
